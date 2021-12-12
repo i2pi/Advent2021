f1fef695532140a74cdc20b4f5d4256211972b64
Josh Reich <josh@i2pi.com>
2021-12-12 23:06:56 +1100

    slightly faster

--- day12.R
+++ day12.R
@@ -1,6 +1,8 @@
 d <- readLines('~/Desktop/Advent2021/data/day12.txt')
 
 nodeNames <- unique(unlist(strsplit(d, '-')))
+
+# Create adjacency matrix
 N <- length(nodeNames)
 a <- matrix(nrow=N, ncol=N, 0)
 colnames(a) <- rownames(a) <- nodeNames
@@ -8,43 +10,37 @@ x<-lapply(strsplit(d, '-'), \(l) a[l[1],l[2]] <<- a[l[2],l[1]] <<- 1)
 
 
 dfs <- function(cur, a, prev, limit) {
-  nexts <- names(which(a[cur,]==1))
+  # Depth first search
+  
+  # Create a vector (vp) of previously visited nodes that can't be revisted
   vp <- prev[toupper(prev) != prev]
   if (length(vp) > 0) {
-    vp <- aggregate(rep(1, length(vp)), list(nodeName=vp), sum)
-    # TODO: This doesn't capture all the double visits...
-    if (any(vp$x > limit)) {
-      vp <- vp$nodeName
+    vpCount <- aggregate(rep(1, length(vp)), list(nodeName=vp), sum)
+    if (any(vpCount$x > limit)) {
+      vp <- vpCount$nodeName
     } else {
-      vp <- vp$nodeName[vp$x > limit]  
+      vp <- vpCount$nodeName[vpCount$x > limit]  
     }
   }
+  if (cur %in% vp) return()
   
+  nexts <- names(which(a[cur,]==1))
   nexts <- nexts[!(nexts %in% vp)]
   nexts <- nexts[nexts != 'start']
     
-  for (n in nexts) if (n != 'end') dfs(n, a, c(prev, cur), limit) else {
+  for (n in nexts) if (n != 'end') {
+    dfs(n, a, c(prev, cur), limit) 
+  } else {
     paths[[length(paths)+1]] <<- c(prev, cur, 'end')
-    pathCount <<- pathCount + 1
   }
 }
 
 # Part 1
 paths <- list()
-pathCount <- 0
 dfs('start', a, NULL, 0)
-pathCount
+length(paths)
 
 # Parts 2
 paths <- list()
-pathCount <- 0
 dfs('start', a, NULL, 1)
-pathCount
-
-# Only allow one double visit to a small cave
-paths <- paths[sapply(paths, \(p) {
-  x <- p[tolower(p) == p]
-  sum(aggregate(rep(1,length(x)), list(name=x), sum)$x > 1) <= 1
-})]
-
 length(paths)
